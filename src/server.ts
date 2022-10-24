2df7c4a20411116ded85fdcbc3167891713bfd1c
hassanrodrigues <hassanrodrigues14@gmail.com>
2022-10-24 03:05:16 -0400

    1 test falied

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import app from "./app";
 import AppDataSource from "./data-source";
+import "dotenv/config"
 
 (async () => {
 
     await AppDataSource.initialize()
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
-    })
-    
-    app.listen(3000, () => {
+        .catch((err) => {
+            console.error("Error during Data Source initialization", err)
+        })
+
+    app.listen(process.env.PORT, () => {
         console.log("Servidor executando")
-    })    
+    })
 })()
